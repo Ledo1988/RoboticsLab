73d21edeb108571a2a87c5d3a1bc4871d431f49d
Elena <elena.do1988@yandex.ru>
2020-02-25 03:20:44 +0300

    mobile is started

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,3 +2,4 @@ import "%modules%/main-slider/main-slider";
 import "%modules%/accordion/accordion";
 import "%modules%/map/map";
 import "%modules%/modal/modal";
+import "%modules%/header-burgermenu/header-burgermenu";
